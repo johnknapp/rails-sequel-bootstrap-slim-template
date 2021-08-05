c0d6cdb5bf1b7f9c2923494b6252dc770b706098
devwojtek <devwojtek99@gmail.com>
2021-08-05 18:25:49 -0400

    fix: remove active_record

--- project/config/application.rb
+++ project/config/application.rb
@@ -1,6 +1,16 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +28,40 @@ module Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Allowed options: :sql, :ruby.
+    config.sequel.schema_format = :sql
+
+    # Allowed options: true, false, default false
+    config.sequel.allow_missing_migration_files = true
+
+    # Whether to dump the schema after successful migrations.
+    # Defaults to false in production and test, true otherwise.
+    config.sequel.schema_dump = true
+
+    # These override corresponding settings from the database config.
+    config.sequel.max_connections = 16
+    config.sequel.search_path = %w(mine public)
+
+    # Configure whether database's rake tasks will be loaded or not.
+    #
+    # If passed a String or Symbol, this will replace the `db:` namespace for
+    # the database's Rake tasks.
+    #
+    # ex: config.sequel.load_database_tasks = :sequel
+    #     will results in `rake db:migrate` to become `rake sequel:migrate`
+    #
+    # Defaults to true
+    # config.sequel.load_database_tasks = false
+
+    # This setting disabled the automatic connect after Rails init
+    config.sequel.skip_connect = true
+
+    # Configure if Sequel should try to 'test' the database connection in order
+    # to fail early
+    config.sequel.test_connect = true
+
+    # If you want to use a specific logger
+    config.sequel.logger = Logger.new($stdout)
   end
 end
